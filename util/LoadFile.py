5a041b15295dfed2bfe9ba63287265c3a8d7c8cf
iBlitzkriegi <iBlitzkriegi@users.noreply.github.com>
2019-08-24 20:31:01 -0400

    Add keyword args for more customization

--- util/LoadFile.py
+++ util/LoadFile.py
@@ -1,16 +1,20 @@
 import json
 from os import path
 
+add_button = {"icon": "plus_button.png", "exe": "Add Button"}
+category_add_button = {"name": "Add Button", "icon": "plus_button.png"}
+
 
 class FileLoader:
     def __init__(self, file='data.json', key='categories'):
         self.file = file
         self.key = key
-        self.data = []
-        self.items = []
+        print(self.key)
+        self.load_file()
 
     def load_file(self):
         if not path.exists('data.json'):
+            print('no')
             file = open('data.json', 'w')
             self.data = {
                 "categories": [],
@@ -30,9 +34,9 @@ class FileLoader:
             f.close()
         return self.data
 
-    def dump_data(self, items, **kwargs):
+    def dump_data(self, **kwargs):
         with open('data.json', 'w') as f:
-            self.data[self.key] = items
+            self.data[self.key] = self.items
             json.dump(self.data, f, indent=4)
             f.close()
         return self.data
@@ -49,7 +53,6 @@ class FileLoader:
                 "add-game": False
             }
         }
-        print(self.data['tutorials'])
 
     def set_key(self, text):
         self.key = text
@@ -61,7 +64,7 @@ class FileLoader:
     def is_first_open(self):
         return self.data['tutorials']['first-open']
 
-    def get_items(self):
+    def get_items(self, **kwargs):
         if self.items is None:
-            return self.load_file()
+            return self.load_file()[self.key]
         return self.items
